42973b0cc075e5e2ace7b5fc976260a7ce531362
suhas mandrawadkar <suhas.sm@gmail.com>
2021-04-11 18:31:04 +1000

    Added themes and typography

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -17,27 +17,45 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
+
+private val NunitoBold = FontFamily(Font(R.font.nunitosans_bold))
+private val NunitoSemiBold = FontFamily(Font(R.font.nunitosans_semibold))
+private val NunitoLight = FontFamily(Font(R.font.nunitosans_light))
 
 // Set of Material typography styles to start with
 val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = NunitoBold,
+        fontSize = 18.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = NunitoBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.15.sp,
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = NunitoLight,
+        fontSize = 16.sp,
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-        /* Other default text styles to override
+        fontFamily = NunitoLight,
+        fontSize = 14.sp,
+    ),
+    body2 = TextStyle(
+        fontFamily = NunitoLight,
+        fontSize = 12.sp,
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = NunitoSemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 1.sp,
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = NunitoSemiBold,
+        fontSize = 12.sp,
     )
-    */
 )
